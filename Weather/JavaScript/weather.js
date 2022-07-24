0d09773bcc758661002b1a08b38b76835deab987
KimSeYoung <sagekim6@gmail.com>
2022-07-24 20:17:20 +0900

    Add convert click event and styling

--- Weather/JavaScript/weather.js
+++ Weather/JavaScript/weather.js
@@ -1,5 +1,7 @@
 import API_KEY from "../key.js";
 
+let isClick = false;
+
 function onSuccess(location) {
   const lat = location.coords.latitude;
   const lon = location.coords.longitude;
@@ -14,18 +16,29 @@ function onSuccess(location) {
   fetch(currentweatherURL)
     .then((response) => response.json())
     .then((data) => {
-      const temp = Math.round(data.main.temp);
+      const temp = Math.round(data.main.temp); // 기본 단위: 섭씨
       const humidity = data.main.humidity;
       const location = data.name;
       const description = data.weather[0].description;
       const icon = data.weather[0].icon;
       const sunrise = data.sys.sunrise;
       const sunset = data.sys.sunset;
+      const fahrenheit = temp * 1.8 + 32;
 
-      curTemp.textContent = `${temp} ℃`;
+      curTemp.textContent = `${temp} °C`;
       humidEl.textContent = `${humidity} %`;
       locationEl.textContent = location;
       descriptionEl.textContent = description;
+
+      curTemp.addEventListener("click", () => {
+        if (!isClick) {
+          curTemp.textContent = `${fahrenheit} °F`;
+          isClick = !isClick;
+        } else {
+          curTemp.textContent = `${temp} °C`;
+          isClick = !isClick;
+        }
+      });
 
       showIcon(icon);
       showAirCondition(lat, lon, API_KEY);
